3deb9b92c1e0e16a1921c9ab52592c14b8966a2c
abdullah al amin <abdullah.alamin@selise.ch>
2022-01-22 22:33:47 +0600

    new update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 
@@ -20,7 +21,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
-    FlexLayoutModule 
+    FlexLayoutModule ,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
